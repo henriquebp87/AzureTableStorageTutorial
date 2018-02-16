da8b6bc18f310e045c692c943357c81467061d39
Henrique Pereira <heinrichpereira@hotmail.com>
2018-02-16 19:38:58 -0200

    Adding project files

---
+++ AzureTableStorageTutorial/Program.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Table; // Namespace for Table storage types
+using System;
+
+namespace AzureTableStorageTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client. Enables you to retrieve tables and entities stored in Table storage
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference("people");
+
+            //SingleInsert(table);
+            //BatchInsert(table);
+            //GetAllByPartitionKey(table);
+            //GetRangeByPartitionKey(table);
+            GetByPartitionKeyAndRowKey(table);
+        }
+
+        private static void GetByPartitionKeyAndRowKey(CloudTable table)
+        {
+            // Create a retrieve operation that takes a customer entity.
+            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerEntity>("Smith", "Ben");
+
+            // Execute the retrieve operation.
+            TableResult retrievedResult = table.Execute(retrieveOperation);
+
+            // Print the phone number of the result.
+            if (retrievedResult.Result != null)
+            {
+                Console.WriteLine(((CustomerEntity)retrievedResult.Result).PhoneNumber);
+            }
+            else
+            {
+                Console.WriteLine("The phone number could not be retrieved.");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void GetRangeByPartitionKey(CloudTable table)
+        {
+            // Create the table query.
+            TableQuery<CustomerEntity> rangeQuery = new TableQuery<CustomerEntity>().Where(
+                TableQuery.CombineFilters(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "Smith"),
+                    TableOperators.And,
+                    TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.LessThan, "E")));
+
+            // Loop through the results, displaying information about the entity.
+            foreach (CustomerEntity entity in table.ExecuteQuery(rangeQuery))
+            {
+                Console.WriteLine("{0}, {1}\t{2}\t{3}", entity.PartitionKey, entity.RowKey,
+                    entity.Email, entity.PhoneNumber);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void GetByPartitionKey(CloudTable table)
+        {
+            // Construct the query operation for all customer entities where PartitionKey="Smith".
+            TableQuery<CustomerEntity> query = new TableQuery<CustomerEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "Smith"));
+
+            // Print the fields for each customer.
+            foreach (CustomerEntity entity in table.ExecuteQuery(query))
+            {
+                Console.WriteLine("{0}, {1}\t{2}\t{3}", entity.PartitionKey, entity.RowKey, entity.Email, entity.PhoneNumber);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void BatchInsert(CloudTable table)
+        {
+            // Create the batch operation.
+            TableBatchOperation batchOperation = new TableBatchOperation();
+
+            // Create a customer entity and add it to the table.
+            CustomerEntity customer1 = new CustomerEntity("Smith", "Jeff");
+            customer1.Email = "Jeff@contoso.com";
+            customer1.PhoneNumber = "425-555-0104";
+
+            // Create another customer entity and add it to the table.
+            CustomerEntity customer2 = new CustomerEntity("Smith", "Ben");
+            customer2.Email = "Ben@contoso.com";
+            customer2.PhoneNumber = "425-555-0102";
+
+            // Add both customer entities to the batch insert operation.
+            batchOperation.Insert(customer1);
+            batchOperation.Insert(customer2);
+
+            // Execute the batch operation.
+            table.ExecuteBatch(batchOperation);
+        }
+
+        private static void SingleInsert(CloudTable table)
+        {
+            // Create the table if it doesn't exist.
+            table.CreateIfNotExists();
+
+            // Create a new customer entity.
+            CustomerEntity customer1 = new CustomerEntity("Harp", "Walter")
+            {
+                Email = "Walter@contoso.com",
+                PhoneNumber = "425-555-0101"
+            };
+
+            // Create the TableOperation object that inserts the customer entity.
+            TableOperation insertOperation = TableOperation.Insert(customer1);
+
+            // Execute the insert operation.
+            table.Execute(insertOperation);
+        }
+    }
+}
